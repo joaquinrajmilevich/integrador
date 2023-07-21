0ec14ce35efa9af6ee4f21d0f998620930e28931
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2023-07-21 19:20:12 +0200

    update next

--- components/MainImage.jsx
+++ components/MainImage.jsx
@@ -2,21 +2,17 @@
 import Image from "next/image";
 export default function MainImage({ children = "", srcImg }) {
   return (
-    <div>
+    <>
       <Image
         src={srcImg}
         fill
-        sizes="(min-width: 808px) 50vw, 100vw"
-        style={{
-          objectFit: "cover",
-        }}
         quality={80}
         priority
         alt="Main Image"
-        className="transition-opacity opacity-0 duration-[0.3s]"
+        className="transition-opacity opacity-0 duration-[0.3s] object-cover"
         onLoadingComplete={(image) => image.classList.remove("opacity-0")}
       />
       {children}
-    </div>
+    </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export",
   basePath: "/integrador",
   assetPrefix: "/integrador",
+  images: {
+    minimumCacheTTL: 31536000,
+  },
 };
 
 module.exports = nextConfig;
