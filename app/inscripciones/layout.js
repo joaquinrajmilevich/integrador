99f82502ca5a5cfb0d8120218c637855ec30d391
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2023-07-21 12:47:28 +0200

    conversion to server components

--- app/inscripciones/layout.js
+++ app/inscripciones/layout.js
@@ -1,20 +1,17 @@
-"use client";
 import "../globals.css";
-
 import Navbar from "@/components/Navbar";
 import { Montserrat } from "next/font/google";
 const font = Montserrat({ subsets: ["latin"] });
-import SecondaryAnimation from "@/components/SecondaryAnimation";
+export const metadata = {
+  title: "Inscripciones / ProyectoGC",
+  description: "inscripciones",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>Inscripciones / ProyectoGC</title>
-        <meta name="description" content="inscripciones" />
-      </head>
       <body className={font.className}>
         <Navbar />
-        <SecondaryAnimation y={-100}>{children}</SecondaryAnimation>
+        {children}
       </body>
     </html>
   );
