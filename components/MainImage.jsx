9f2c0ceb606a780328a026405557ac4d5443a05c
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2023-07-24 23:10:03 +0200

    fix MainImage children

--- components/MainImage.jsx
+++ components/MainImage.jsx
@@ -1,15 +1,20 @@
 "use client";
 import Image from "next/image";
-export default function MainImage({ srcImg }) {
+export default function MainImage({ children, srcImg }) {
   return (
-    <Image
-      src={srcImg}
-      fill
-      loading="eager"
-      priority
-      alt="Main Image"
-      className="transition-opacity opacity-0 duration-[0.2s] object-cover"
-      onLoadingComplete={(image) => image.classList.remove("opacity-0")}
-    />
+    <div className="transition-opacity opacity-0 duration-[0.2s]">
+      <Image
+        src={srcImg}
+        fill
+        priority
+        loading="eager"
+        alt="Main Image"
+        className="object-cover"
+        onLoadingComplete={(image) =>
+          image.parentElement.classList.remove("opacity-0")
+        }
+      />
+      {children}
+    </div>
   );
 }
