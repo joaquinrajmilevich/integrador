3db16e04f160ccfd1a77b043c7040bdf595b29f3
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2023-07-25 20:25:07 +0200

    test pathname footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import { usePathname } from "next/navigation";
 import Footer from "./Footer";
 const Layout = () => {
   const pathname = usePathname();
-  const shouldRender = pathname !== "/inscripciones";
+  const shouldRender = pathname !== "/integrador/inscripciones";
   return <>{shouldRender ? <Footer /> : <></>}</>;
 };
 
